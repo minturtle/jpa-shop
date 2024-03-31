780a92152b78b9182543d1aa50b4be4568767046
minturtle <minseok0425@kakao.com>
2024-03-31 14:25:03 +0900

    Test : 생성된 JWT토큰을 해독할 수 있다

--- src/test/java/jpabook/jpashop/util/JwtTokenProviderTest.java
+++ src/test/java/jpabook/jpashop/util/JwtTokenProviderTest.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop.util;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -33,5 +35,36 @@ class JwtTokenProviderTest {
         assertThat(token).isNotNull();
     }
 
+    @Test
+    @DisplayName("생성된 JWT토큰을 해독해 UID를 획득할 수 있다.")
+    public void testVerify() throws Exception{
+        //given
+        String givenUid = "givenUid";
+        Date createTime = new Date();
+
+        String token = jwtTokenProvider.sign(givenUid, createTime);
+        //when
+        String actual = jwtTokenProvider.verify(token);
+        //then
+        assertThat(actual).isEqualTo(givenUid);
+
+    }
+
+    @Test
+    @DisplayName("만료된 토큰을 해독하는 경우 ExpiredTokenException을 throw할 수 있다.")
+    public void testVerifyExpiredToken() throws Exception{
+        //given
+        String givenUid = "givenUid";
+        Date createTime = new Date(1000000000000L); //약 2001년 9월
+
+        String expiredToken = jwtTokenProvider.sign(givenUid, createTime);
+        //when
+
+        ThrowableAssert.ThrowingCallable callable = ()->jwtTokenProvider.verify(expiredToken);
+
+        //then
+        assertThatThrownBy(callable)
+                .isInstanceOf(ExpiredJwtException.class);
+    }
 
 }
