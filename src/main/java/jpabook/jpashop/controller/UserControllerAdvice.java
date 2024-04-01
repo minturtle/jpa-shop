ce6918caddca7bb9bd223a6f3c1173ebf3924bbd
minturtle <minseok0425@kakao.com>
2024-04-01 23:21:08 +0900

    Feat : UserControllerAdvice의 basePackage 설정 및 OptimisticLockingFailureException 처리 추가

--- src/main/java/jpabook/jpashop/controller/UserControllerAdvice.java
+++ src/main/java/jpabook/jpashop/controller/UserControllerAdvice.java
@@ -7,6 +7,7 @@ import jpabook.jpashop.controller.response.ErrorResponse;
 import jpabook.jpashop.exception.user.AlreadyExistsUserException;
 import jpabook.jpashop.exception.user.AuthenticateFailedException;
 import jpabook.jpashop.exception.user.PasswordValidationException;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.Arrays;
 
 
-@RestControllerAdvice
+@RestControllerAdvice(basePackageClasses = UserController.class)
 public class UserControllerAdvice {
 
     @ExceptionHandler({ PasswordValidationException.class, AlreadyExistsUserException.class, JwtException.class})
@@ -28,6 +29,12 @@ public class UserControllerAdvice {
     public ResponseEntity<ErrorResponse> UnAuthorized(Exception e){
         return new ResponseEntity<>(new ErrorResponse(e.getMessage(), Arrays.toString(e.getStackTrace())), HttpStatus.UNAUTHORIZED);
     }
+
+    @ExceptionHandler({OptimisticLockingFailureException.class})
+    public ResponseEntity<ErrorResponse> Conflict(Exception e){
+        return new ResponseEntity<>(new ErrorResponse(e.getMessage(), Arrays.toString(e.getStackTrace())), HttpStatus.CONFLICT);
+    }
+
 
 }
 
