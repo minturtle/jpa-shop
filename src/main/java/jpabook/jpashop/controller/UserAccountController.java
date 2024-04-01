0d6733763111db37fc0612dfe0d064dedd24f233
minturtle <minseok0425@kakao.com>
2024-04-01 23:56:18 +0900

    test-green : 회원 인증이 완료된 유저가 새로운 계좌를 추가해 잔고가 0원인 채로 DB에 저장할 수 있다.

--- src/main/java/jpabook/jpashop/controller/UserAccountController.java
+++ src/main/java/jpabook/jpashop/controller/UserAccountController.java
@@ -3,6 +3,8 @@ package jpabook.jpashop.controller;
 
 import jpabook.jpashop.controller.argumentResolvers.annotations.LoginedUserUid;
 import jpabook.jpashop.controller.response.UserAccountResponse;
+import jpabook.jpashop.dto.AccountDto;
+import jpabook.jpashop.exception.common.CannotFindEntityException;
 import jpabook.jpashop.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,8 +21,10 @@ public class UserAccountController {
 
 
     @PostMapping("")
-    public UserAccountResponse.Create addAccount(@LoginedUserUid String userUid){
-        return null;
+    public UserAccountResponse.Create addAccount(@LoginedUserUid String userUid) throws CannotFindEntityException {
+        String accountUid = accountService.addAccount(new AccountDto.Create(userUid, 0L));
+
+        return new UserAccountResponse.Create(accountUid);
     }
 
 }
