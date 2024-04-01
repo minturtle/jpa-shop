8dfb2ff3cb128e8c066290d185679e7cca4dbbe3
minturtle <minseok0425@kakao.com>
2024-04-01 23:35:34 +0900

    Test-Green : 사용자가 비밀번호 변경 시도시 username/password가 설정되지 않은 유저라면 403 오류를 throw하며 DB에 업데이트되지 않는다.

--- src/main/java/jpabook/jpashop/controller/UserControllerAdvice.java
+++ src/main/java/jpabook/jpashop/controller/UserControllerAdvice.java
@@ -7,6 +7,7 @@ import jpabook.jpashop.controller.response.ErrorResponse;
 import jpabook.jpashop.exception.user.AlreadyExistsUserException;
 import jpabook.jpashop.exception.user.AuthenticateFailedException;
 import jpabook.jpashop.exception.user.PasswordValidationException;
+import jpabook.jpashop.exception.user.UserAuthTypeException;
 import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,12 +27,17 @@ public class UserControllerAdvice {
 
 
     @ExceptionHandler({ExpiredJwtException.class, AuthenticateFailedException.class})
-    public ResponseEntity<ErrorResponse> UnAuthorized(Exception e){
+    public ResponseEntity<ErrorResponse> unAuthorized(Exception e){
         return new ResponseEntity<>(new ErrorResponse(e.getMessage(), Arrays.toString(e.getStackTrace())), HttpStatus.UNAUTHORIZED);
     }
 
+    @ExceptionHandler({UserAuthTypeException.class})
+    public ResponseEntity<ErrorResponse> forbidden(Exception e){
+        return new ResponseEntity<>(new ErrorResponse(e.getMessage(), Arrays.toString(e.getStackTrace())), HttpStatus.FORBIDDEN);
+    }
+
     @ExceptionHandler({OptimisticLockingFailureException.class})
-    public ResponseEntity<ErrorResponse> Conflict(Exception e){
+    public ResponseEntity<ErrorResponse> conflict(Exception e){
         return new ResponseEntity<>(new ErrorResponse(e.getMessage(), Arrays.toString(e.getStackTrace())), HttpStatus.CONFLICT);
     }
 
