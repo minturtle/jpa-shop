499b4d1d2128cb5e97a9e0bec5b777860d244cbb
minturtle <minseok0425@kakao.com>
2024-04-05 20:50:26 +0900

    Test-red : 사용자가 장바구니에 이미 있는 상품을 추가할시 기존 장바구니에 있던 상품의 갯수가 증가한다.

--- src/test/java/jpabook/jpashop/controller/ProductCartControllerTest.java
+++ src/test/java/jpabook/jpashop/controller/ProductCartControllerTest.java
@@ -81,6 +81,37 @@ class ProductCartControllerTest {
     }
 
     @Test
+    @DisplayName("사용자가 장바구니에 이미 있는 상품을 추가할시 기존 장바구니에 있던 상품의 갯수가 증가한다.")
+    public void testWhenAddCartAlreadyExistsThenAddCartQuantity() throws Exception{
+        //given
+        String givenUid = "user-001";
+        String givenProductUid = "album-001";
+        int givenQuantity = 1;
+
+        String token = tokenProvider.sign(givenUid, new Date());
+
+        //when
+        mockMvc.perform(post("/api/product/cart")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new CartRequest.Add(givenProductUid, givenQuantity))))
+                .andDo(print())
+                .andExpect(status().isOk());
+        //then
+        User user = userRepository.findByUid(givenUid)
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+
+        assertThat(user.getCartList().size()).isEqualTo(2);
+        assertThat(user.getCartList()).extracting("product.uid", "quantity")
+                .contains(
+                        tuple("album-001", 4),
+                        tuple("book-001", 2)
+                );
+
+    }
+
+
+    @Test
     @DisplayName("사용자는 자신의 장바구니에 속한 상품을 조회할 수 있다.")
     public void testWhenGetCategorylistThenReturn() throws Exception{
         //given
