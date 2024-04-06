d978e33debdfc2a0db6df01879f913770f24a170
minturtle <minseok0425@kakao.com>
2024-04-06 13:12:33 +0900

    Feat : Controller Advice 로그 추가

--- src/main/java/jpabook/jpashop/controller/order/OrderControllerAdvice.java
+++ src/main/java/jpabook/jpashop/controller/order/OrderControllerAdvice.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.controller.order;
 import jpabook.jpashop.controller.common.response.ErrorResponse;
 import jpabook.jpashop.exception.product.InvalidStockQuantityException;
 import jpabook.jpashop.exception.user.account.InvalidBalanceValueException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
@@ -14,10 +15,12 @@ import java.util.Arrays;
 
 @RestControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
+@Slf4j
 public class OrderControllerAdvice {
 
     @ExceptionHandler({InvalidBalanceValueException.class, InvalidStockQuantityException.class})
     public ResponseEntity<ErrorResponse> badRequest(Exception e){
+        log.info("method execution failed-400 : {}", e.getMessage());
         return new ResponseEntity<>(new ErrorResponse(e.getMessage(), Arrays.toString(e.getStackTrace())), HttpStatus.BAD_REQUEST);
     }
 
