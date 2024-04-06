1811d5619cc428afdcc931f78bdb5bb9fe3d3aee
minturtle <minseok0425@kakao.com>
2024-04-06 13:03:27 +0900

    Feat : 시간 측정 AOP 추가

---
+++ src/main/java/jpabook/jpashop/aop/LoggingAspect.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.aop;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+
+    @Pointcut("@within(jpabook.jpashop.aop.annotations.Loggable)")
+    public void loggableClass() {
+    }
+
+    @Around("loggableClass()")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        log.info("Executing method: {}", joinPoint.getSignature().toShortString());
+
+        Object proceed = joinPoint.proceed();
+
+        long executionTime = System.currentTimeMillis() - start;
+        log.info("{} executed in {}ms", joinPoint.getSignature().toShortString(), executionTime);
+        return proceed;
+    }
+
+
+
+}
