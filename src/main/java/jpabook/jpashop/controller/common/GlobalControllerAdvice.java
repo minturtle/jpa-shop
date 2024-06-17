f9a7fdd12802f89ddf324bd714a5f8a45006f9b4
minturtle <minseok0425@kakao.com>
2024-06-17 21:18:54 +0900

    Refactor : GlobalControllerAdvice 주석 추가 #30

--- src/main/java/jpabook/jpashop/controller/common/GlobalControllerAdvice.java
+++ src/main/java/jpabook/jpashop/controller/common/GlobalControllerAdvice.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.controller.common;
 
 import io.jsonwebtoken.ExpiredJwtException;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jpabook.jpashop.controller.common.response.ErrorResponse;
 import jpabook.jpashop.exception.common.CannotFindEntityException;
 import jpabook.jpashop.exception.common.InternalErrorException;
@@ -18,6 +19,12 @@ import java.util.Arrays;
 public class GlobalControllerAdvice {
 
 
+    /**
+     * @author minseok kim
+     * @description Entity의 ID(UID)를 입력받았으나, Entity를 조회할 수 없는 경우 실행되는 메서드
+     * @param e Exception 객체
+     * @return 404 Http Code With ErrorResponse Object
+    */
     @ExceptionHandler({CannotFindEntityException.class})
     public ResponseEntity<ErrorResponse> notfound(Exception e){
         log.info("method execution failed-404 : {}", e.getMessage());
@@ -25,11 +32,25 @@ public class GlobalControllerAdvice {
     }
 
 
+
+    /**
+     * @author minseok kim
+     * @description 낙관적 락 충돌, 즉 동시성 버전 오류 발생시 실행되는 메서드
+     * @param e Exception 객체
+     * @return 409 Http Code With ErrorResponse Object
+    */
     @ExceptionHandler({OptimisticLockingFailureException.class})
     public ResponseEntity<ErrorResponse> conflict(Exception e){
         log.info("method execution failed-409 : {}", e.getMessage());
         return new ResponseEntity<>(new ErrorResponse(e.getMessage(), Arrays.toString(e.getStackTrace())), HttpStatus.CONFLICT);
     }
+
+    /**
+     * @author minseok kim
+     * @description JWT Token 만료시 나타나는 메서드
+     * @param e Exception 객체
+     * @return 401 Http Code With ErrorResponse Object
+    */
     @ExceptionHandler({ExpiredJwtException.class})
     public ResponseEntity<ErrorResponse> unAuthorized(Exception e){
         log.info("method execution failed-401 : {}", e.getMessage());
@@ -37,6 +58,12 @@ public class GlobalControllerAdvice {
     }
 
 
+    /**
+     * @author minseok kim
+     * @description 기타 서버에서 발생한 예상치못한 Exception
+     * @param e Exception 객체
+     * @return 500 Http Code With ErrorResponse Object
+    */
     @ExceptionHandler({InternalErrorException.class, Exception.class})
     public ResponseEntity<ErrorResponse> unexpectedException(Exception e){
         log.warn("method execution failed-500 : {} - unexpected exception", e.getMessage());
