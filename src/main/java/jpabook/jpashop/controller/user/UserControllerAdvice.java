67c3e7e9be7b7205b0a762de76f39d7dcd41e9a4
minturtle <minseok0425@kakao.com>
2024-06-18 00:11:36 +0900

    Feat : ControllerAdvice ApiResponse 추가 #30

--- src/main/java/jpabook/jpashop/controller/user/UserControllerAdvice.java
+++ src/main/java/jpabook/jpashop/controller/user/UserControllerAdvice.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.controller.user;
 
 
 import io.jsonwebtoken.JwtException;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jpabook.jpashop.controller.common.response.ErrorResponse;
 import jpabook.jpashop.exception.user.AlreadyExistsUserException;
 import jpabook.jpashop.exception.user.AuthenticateFailedException;
@@ -26,6 +28,9 @@ import java.util.Arrays;
 public class UserControllerAdvice {
 
     @ExceptionHandler({ PasswordValidationException.class, AlreadyExistsUserException.class, JwtException.class, InvalidBalanceValueException.class})
+    @ApiResponses({
+            @ApiResponse(responseCode = "400")
+    })
     public ResponseEntity<ErrorResponse> badRequest(Exception e){
         log.info("method execution failed-400 : {}", e.getMessage());
         return new ResponseEntity<>(new ErrorResponse(e.getMessage(), Arrays.toString(e.getStackTrace())), HttpStatus.BAD_REQUEST);
@@ -33,12 +38,18 @@ public class UserControllerAdvice {
 
 
     @ExceptionHandler({AuthenticateFailedException.class})
+    @ApiResponses({
+            @ApiResponse(responseCode = "401")
+    })
     public ResponseEntity<ErrorResponse> unAuthorized(Exception e){
         log.info("method execution failed-401 : {}", e.getMessage());
         return new ResponseEntity<>(new ErrorResponse(e.getMessage(), Arrays.toString(e.getStackTrace())), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler({UserAuthTypeException.class, UnauthorizedAccountAccessException.class})
+    @ApiResponses({
+            @ApiResponse(responseCode = "403")
+    })
     public ResponseEntity<ErrorResponse> forbidden(Exception e){
         log.info("method execution failed-403 : {}", e.getMessage());
         return new ResponseEntity<>(new ErrorResponse(e.getMessage(), Arrays.toString(e.getStackTrace())), HttpStatus.FORBIDDEN);
