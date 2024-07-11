0d1b9015cca471da881bd068a3bc8d2181b3d848
minturtle <minseok0425@kakao.com>
2024-07-11 23:12:28 +0900

    feat: 상품 메인 조회 페이지 UI 개발 완료

---
+++ src/main/resources/static/js/product/detail-scripts.js
@@ -0,0 +1,9 @@
+function addToCart(uid) {
+    // 장바구니에 추가하는 로직 구현
+    alert("상품이 장바구니에 추가되었습니다: " + uid);
+}
+
+function orderProduct(uid) {
+    // 주문하는 로직 구현
+    alert("상품이 주문되었습니다: " + uid);
+}
