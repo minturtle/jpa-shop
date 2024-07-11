0ef924b94ef6ee6a3503e086a8fc5e47293b28a5
minturtle <minseok0425@kakao.com>
2024-07-11 23:28:07 +0900

    feat: 뒤로가기 버튼 추가

--- src/main/java/jpabook/jpashop/controller/ui/home/HomeUIController.java
+++ src/main/java/jpabook/jpashop/controller/ui/home/HomeUIController.java
@@ -2,14 +2,18 @@ package jpabook.jpashop.controller.ui.home;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeUIController {
 
     @GetMapping("/")
-    public String homePage(){
+    public String homePage(Model model)
+    {
+        model.addAttribute("pageType", "home");
         return "home";
+
     }
 
 }
